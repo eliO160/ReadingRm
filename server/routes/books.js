3e32bf6f5b785599d150f520f822f62c6ffac65c
eliO160 <lizovieda@gmail.com>
2025-10-07 21:31:00 -0700

    Refactored server.js. created basic user model.

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,13 +1,11 @@
-//routes -- define endpoints
+import express from "express";
+const router = express.Router();
 
-import express from 'express';
 import { 
   searchBooks,
   getBookById,
   getBookHtml
 } from '../controllers/booksController.js';
-
-const router = express.Router();
 
 router.get('/html/:id', getBookHtml); // GET example: .../api/books/html/11
 router.get('/:id', getBookById); // GET example: .../api/books/12
