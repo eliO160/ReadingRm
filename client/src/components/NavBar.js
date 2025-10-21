72cf4f7d6d01277d612a4795552610b700bcbef3
eliO160 <lizovieda@gmail.com>
2025-10-20 22:45:51 -0700

    Created demo page that serves html and cover image locally without a web API call

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,6 +9,7 @@ export default function NavBar() {
     <nav className="flex flex-row justify-end gap-10 px-10 py-3">
       <SearchBar />
       <Link href="/about">About</Link>
+      <Link href="/demo_page">Demo Page</Link>
       <Link href="/search">Advanced Search</Link>
       <Link href="/signIn">Sign In</Link>
       <ThemeToggle />
